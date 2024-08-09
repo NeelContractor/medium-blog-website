032b485be204bab8fabbd4e297ea7b1d23cba55e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-08-09 14:26:16 +0530

    added frontend

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
-import { createBlogInput, CreateBlogInput, updateBlogInput } from "@neel3/medium-common"
+import { createBlogInput, updateBlogInput } from "@neel3/medium-common"
 
 export const blogRouter = new Hono<{
     Bindings: {
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.neelcontractor7.workers.dev/"
